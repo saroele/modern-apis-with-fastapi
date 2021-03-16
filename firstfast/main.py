f16aa9cbbc2031c50e6720e869905620cfcd4b2c
saroele <roeldeconinck@gmail.com>
2021-03-16 20:16:37 +0000

    My first fastAPI API ever hopefully not the last :-)

---
+++ firstfast/main.py
@@ -0,0 +1,36 @@
+from typing import Optional
+import fastapi
+import uvicorn
+
+api = fastapi.FastAPI()
+
+
+@api.get('/')
+def index():
+    body = "<html>" \
+           "<body style='padding: 10px;'>" \
+           "<h1>Welcome to the API</h1>" \
+           "<div>" \
+           "Try it: <a href='/api/calculate?x=7&y=11'>/api/calculate?x=7&y=11</a>" \
+           "</div>" \
+           "</body>" \
+           "</html>"
+
+    return fastapi.responses.HTMLResponse(content=body)
+
+
+@api.get('/api/calculate')
+def calculate(x: int, y: int, z: Optional[int] = None):
+    if z == 0:
+        return fastapi.responses.JSONResponse(
+            content={"Error": "z cannot be zero!"},
+            status_code=400,
+        )
+    
+    value = (x+y)
+    if z is not None:
+        value /= z
+    return {'value': value}
+
+
+uvicorn.run(api, host='127.0.0.1', port=8000)
